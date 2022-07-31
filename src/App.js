75fd87a5b34417d75daef4114087f2920dd630d2
kurniadikevin <kurniadikevin@gmail.com>
2022-07-31 15:21:29 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+
+class App extends React.Component {
+  
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>hello</p>
+      <div className="a-section">
+          <form>
+            <label>Name </label>
+            <input type='text'></input> <br></br>
+            <label>Email </label>
+            <input type="email"></input><br></br>
+            <label>Phone </label>
+            <input type="number"></input>
+          </form>
+      </div>
+
+
+      <div className="b-section">
+        <form>
+          <label>Education</label>
+          <input type="text"></input><br></br>
+          <label>School Name</label>
+          <input type="text"></input>
+          <labe>Year</labe>
+          <input type="number"></input><br></br>
+        </form>
+      </div>
+
+      <div className="c-section">
+        <form>
+          <label>Experience</label>
+          <input type="text"></input><br></br>
+          <label>From</label><input type="month"></input>
+          <label>To</label><input type='month'></input>
+        </form>
+      </div>
+      
     </div>
   );
+}
 }
 
 export default App;
