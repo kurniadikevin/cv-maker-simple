af3ba5574e5036b9f2db5e9dfbdf5188373e6c2b
kurniadikevin <kurniadikevin@gmail.com>
2023-04-10 15:24:09 +0700

    change remote url on local repo

--- src/component/display.js
+++ src/component/display.js
@@ -8,14 +8,30 @@ const Display = (props) =>{
     
         return(
             <div className="cv-display">
+               <div>
                <div className="title">Name</div><div>{name}</div>
+               </div>
+               <div>
                <div className="title">Email</div><div>{email}</div>
+               </div>
+               <div>
                <div className="title">Phone</div><div>{phone}</div>
+               </div>
+               <div>
                <div className="title">Last education</div><div>{education}</div>
+               </div>
+               <div>
                <div className="title">School name</div><div>{schoolName}</div>
+               </div>
+               <div>
                <div className="title">School Year</div><div>{schoolYear}</div>
+               </div>
+               <div id="exp">
                <div className="title">Experience</div><div>{experience}</div>
+               </div>
+               <div>
                <div className="title">From year: {fromYear}</div><div>To: {toYear}</div>
+               </div>
             </div>
         )
     
